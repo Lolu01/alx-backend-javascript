09f77c8136370c8898b18abc6ee68ed446bfdf17
Lolu01 <jaiyeolalulu@gmail.com>
2024-10-03 22:53:01 +0000

    0x02. ES6 classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,11 +6,24 @@ export default class Pricing {
     this._currency = new Currency(currency.code, currency.name);
   }
 
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    }
+  }
+
   displayFullPrice() {
-    return `${this.amount} ${this._currency._name} (${this._currency._code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
+
